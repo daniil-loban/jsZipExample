a0126934768881a042e356851b9887df20d1280d
daniil-loban <dungeon_lda@mail.ru>
2021-06-07 08:06:39 +0300

    feat: add files

---
+++ examples/example01.cpp
@@ -0,0 +1,11 @@
+// Example program 01
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
